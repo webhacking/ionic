b5200e70b97158140c0dd71567616b5b38326596
Max Lynch <max@drifty.com>
2015-11-27 22:48:11 -0600

    Icon component

--- js/angular/directive/icon.js
+++ js/angular/directive/icon.js
@@ -20,7 +20,60 @@ IonicModule
     },
     controller: '$ionInfiniteScroll',
     link: function($scope, $element, $attrs, ctrls) {
+      var name = $scope.name;
+      if(ionic.Platform.isIOS() && isDefined($attrs.ios)) {
+        name = $attrs.ios;
+      } else if(ionic.Platform.isAndroid() && isDefined($attrs.md)) {
+        name = $attrs.md;
+      } else if(!name) {
+        var ele = $element[0];
+        for(var i = 0, l = ele.attributes.length; i < l; i++) {
+          if (ele.attributes[i].value === '' && /_|item-|is-active|large|small|class/.test(ele.attributes[i].name) !== true) {
+            name = ele.attributes[i].name;
+            break;
+          }
+        }
+      }
+
+      var mode = ionic.Platform.isAndroid() ? 'md' : 'ios';
+      var isActive = true;
+      var _name;
+
+      if (!(/^ion-/.test(name))) {
+        // not an exact icon being used
+        // add mode specific prefix
+        name = 'ion-' + mode + '-' + name;
+      }
+
+      console.log('Icon on platform', $scope.name);
+
       $element.addClass('ion-ios-' + $scope.name);
+
+      update();
+
+      function update() {
+        if (name && mode == 'ios') {
+
+          if (isActive) {
+            if (/-outline/.test(name)) {
+              name = name.replace('-outline', '');
+            }
+          } else if (!(/-outline/.test(name))) {
+            name += '-outline';
+          }
+
+        }
+
+        if (_name !== name) {
+          if (_name) {
+            $element.removeClass(_name);
+          }
+          _name = name;
+          $element.addClass(name);
+
+          $attrs['aria-label'] = name.replace('ion-', '').replace('ios-', '').replace('md-', '').replace('-', ' ');
+        }
+      }
     }
   }
 }]);
